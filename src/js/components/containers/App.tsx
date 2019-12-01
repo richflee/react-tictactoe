66f19ef67edc4ff47f6c57dfd24241776bf89c07
Richard Lee <moltesanti@yahoo.com>
2019-12-01 20:47:20 +1100

    Removed redundant routes and components. Basic game board and game state implemented.

--- src/js/components/containers/App.tsx
+++ src/js/components/containers/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home/Home";
-import About from "./About/About";
-import Contact from "./Contact/Contact";
 require('./App.css')
 
 
@@ -13,7 +11,7 @@ class App extends React.Component<any, AppComponentState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            title: "React Boilerplate3000"
+            title: "Noughts and crosses"
         };
     }
     render() {
@@ -21,21 +19,7 @@ class App extends React.Component<any, AppComponentState> {
             <Router>
                 <div>
                     <h1>{this.state.title}</h1>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-
                     <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/contact" component={Contact} />
                 </div>
             </Router>
         );
--- src/js/components/containers/Home/Home.tsx
+++ src/js/components/containers/Home/Home.tsx
@@ -1,5 +1,115 @@
 import * as React from 'react';
 import Button from '../../components/Button/Button';
+import styled from 'styled-components';
+
+interface BoardCellState {
+    value: 'x' | 'o' | '-'
+}
+
+export class BoardCell extends React.Component<any, BoardCellState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            value: props.value
+        }
+
+        this._onClickHandler = this._onClickHandler.bind(this);
+    }
+
+    _onClickHandler() {
+        console.log('blarry');
+        this.props.onCellClickHandler();
+    }
+
+    render() {
+        return (
+            <button onClick={() => this._onClickHandler()}>{this.state.value}</button>
+        );
+    }
+}
+
+const StyledGameBoard = styled.div`
+    background-color: yellow;
+    border: 1px solid blue;
+    font-size: 1em;
+    > .gameBoardContainer__row {
+        display: flex;
+        
+        > * {
+            border: 1px solid blue;
+            flex-grow: 1;
+            padding: 5px;
+            text-align: center;
+        }
+    }
+`;
+
+interface GameBoardState {
+    board: number[][],
+    currentPlayer: 'x' | 'o'
+}
+
+export class GameBoard extends React.Component<any, GameBoardState> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            board: this._populateGameBoard(),
+            currentPlayer: 'x'
+        } as GameBoardState;
+    }
+
+    _populateGameBoard(): any[] {
+        const columnCount = 3;
+        const rowCount = 3;
+        let board = [];
+
+        for (let i = 0; i < rowCount; i++) {
+            let row = [];
+            for (let j = 0; j < columnCount; j++) {
+                row.push('-');
+            }
+            board.push(row);
+        }
+
+        return board;
+    }
+
+    _toggleCurrentPlayer() {
+        this.setState({
+            currentPlayer: this.state.currentPlayer === 'x' ? 'o' : 'x'
+        });
+    }
+
+    _updateCellState(colIndex: number, rowIndex: number) {
+        return () => {
+            const cp = JSON.parse(JSON.stringify(this.state.board));
+            cp[rowIndex][colIndex] = this.state.currentPlayer;
+            this._toggleCurrentPlayer();
+            this.setState({
+                board: cp
+            });
+        }
+    }
+
+    render() {
+        console.log(this.state.board);
+        return (
+            <div>
+                <div>Game here</div>
+                <StyledGameBoard>
+                    {this.state.board.map((data, idx) => (
+                        <div key={Math.floor(Math.random() * Math.floor(12000))} className="gameBoardContainer__row">
+                            {data.map((colData, index) => (
+                                <BoardCell key={Math.floor(Math.random() * Math.floor(20000))} onCellClickHandler={this._updateCellState(index, idx)} value={colData}></BoardCell>
+                            ))}
+                        </div>
+                    ))}
+                </StyledGameBoard>
+            </div>
+        );
+    }
+}
 
 
 interface HomeComponentState {
@@ -25,29 +135,20 @@ export default class Home extends React.Component<any, HomeComponentState> {
     }
 
     _renderExplanatoryText() {
-        let explanatoryText; 
-        if (this.state.revealExplanatoryInfo) {
-            explanatoryText = `Welcome to my cool React boilerplate. As you can see I'm trying to jam as many useful tools into it, such as: Styled Components, Jest, Typescript and more.`;
-        } else {
-            explanatoryText = ``;
-        }
+        let explanatoryText = ``;
         return <p>{explanatoryText}</p>;
     }
 
     _toggleMessageButtonText() {
-        if (this.state.revealExplanatoryInfo) {
-            return `Hide welcome message`;
-        } else {
-            return `Show welcome message`;
-        }
+        return `New game`;
     }
 
     render() {
         return (
             <div>
-                <h2>Home</h2>
                 <Button onclick={this._onToggleTextDisplay}>{this._toggleMessageButtonText()}</Button>
                 {this._renderExplanatoryText()}
+                <GameBoard></GameBoard>
             </div>
         );
     }
